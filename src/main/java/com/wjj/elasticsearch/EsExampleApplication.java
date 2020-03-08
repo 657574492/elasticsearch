678ea5523790bd6667daa523a44fa088563fb0e2
wangjunjie <122077774@qq.com>
2020-03-08 17:13:12 +0800

    添加距离查询相关

--- src/main/java/com/wjj/elasticsearch/EsExampleApplication.java
+++ src/main/java/com/wjj/elasticsearch/EsExampleApplication.java
@@ -1,6 +1,7 @@
 package com.wjj.elasticsearch;
 
 import com.wjj.elasticsearch.example.config.properties.WjjProperties;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
